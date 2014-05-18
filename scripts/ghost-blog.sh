c26ae901b8fbd04085ac7f5a0b68f60e82748e79
Atindriya Ghosh <atindriya@ghosh.me>
2014-05-18 23:05:42 +0530

    Fixing colour issues

--- scripts/ghost-blog.sh
+++ scripts/ghost-blog.sh
@@ -2,9 +2,9 @@
 # ***********************************************
 # * SCRIPT NAME: ghost-blog.sh                  *
 # *                                             *
-# * VERSION: 1.0.0                              *
+# * VERSION: 1.0.1                              *
 # *                                             *
-# * DATE MODIFIED (MM-DD-YYYY): 05-04-2014      *
+# * DATE MODIFIED (MM-DD-YYYY): 05-18-2014      *
 # *                                             *
 # * AUTHOR: Atindriya Ghosh                     *
 # *                                             *
@@ -20,7 +20,7 @@ usage(){
     echo "1. $(tput setaf 6)./ghost-blog.sh -s$(tput sgr 0) or $(tput setaf 6)./ghost-blog.sh --start$(tput sgr 0)$(tput sgr 0) - Starts the blog"
     echo "2. $(tput setaf 6)./ghost-blog.sh -t$(tput sgr 0) or $(tput setaf 6)./ghost-blog.sh --terminate$(tput sgr 0)$(tput sgr 0) - Stops the blog"
 	echo "3. $(tput setaf 6)./ghost-blog.sh -r$(tput sgr 0) or $(tput setaf 6)./ghost-blog.sh --restart$(tput sgr 0)$(tput sgr 0) - Restarts the blog"
-	echo "4. $(tput setaf 6)./ghost-blog.sh -d$(tput sgr 0) or $(tput setaf 6)./ghost-blog.sh --deploy$(tput sgr 0)$(tput sgr 0) - Deploy the latest release"
+	echo "4. $(tput setaf 6)./ghost-blog.sh -d <Artifact Path>$(tput sgr 0) or $(tput setaf 6)./ghost-blog.sh --deploy <Artifact Path>$(tput sgr 0)$(tput sgr 0) - Deploy the latest release"
 }
 startup(){ 
 	echo "$(tput setaf 3)Starting The Blog ...$(tput sgr 0)"
@@ -46,7 +46,7 @@ deploy(){
 	echo "$(tput setaf 3)Deploying Latest Release Artifacts ...$(tput sgr 0)"
 	cd /var/www/content/themes/the-ghost-who-blogs
 	sudo rm -rf *
-	sudo unzip /home/ubuntu/Ghostion/releases/theme/the-ghost-who-blogs.zip
+	sudo unzip "$1"
 	cd /var/www
 	echo "$(tput setaf 2)Release Artifacts Deployed Successfully$(tput sgr 0)"
 	startup
@@ -57,19 +57,25 @@ while [ "$1" != "" ]; do
     	"-s" | "--start"	)	startup
 							exit
 							;;
-		"-t" | "--terminate"		)	shutdown
+		"-t" | "--terminate")	shutdown
 							exit
 							;;
 		"-r" | "--restart"	)	restart
 							exit
 							;;
-		"-d" | "--deploy"	)	deploy
+		"-d" | "--deploy"	)	
+							if ["$2" == ""]; 
+							then
+								echo "$(tput setaf 1)Please pass release path and artifact path$(tput sgr 0)\n"
+								usage
+							else
+								deploy $2
 							exit
 							;;	
 		"-h" | "--help"		)	usage
 							exit
 							;;
-		*				)	echo "$(tput setaf 1)Please pass the available parameters$(tput sgr 0)\n"
+		*					)	echo "$(tput setaf 1)Please pass the available parameters$(tput sgr 0)\n"
 							usage
 							exit
 	esac
