4f0eaf164a28cbc2c219ba303de712674b45f28e
Atindriya Ghosh <atindriya@ghosh.me>
2014-06-07 17:06:56 +0530

    Modified ghost-blog.sh to add list and clean features

--- scripts/ghost-blog.sh
+++ scripts/ghost-blog.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-# ***********************************************
-# * SCRIPT NAME: ghost-blog.sh                  *
-# *                                             *
-# * VERSION: 1.0.1                              *
-# *                                             *
-# * DATE MODIFIED (MM-DD-YYYY): 05-18-2014      *
-# *                                             *
-# * AUTHOR: Atindriya Ghosh                     *
-# *                                             *
-# * FUNCTIONS:                                  *
-# * 1. Start the blog                           *
-# * 2. Stop the blog                            *
-# * 3. Restart the blog                         *
-# * 4. Deploy the latest release artifacts      *
-# ***********************************************
+# ***************************************************************
+# * SCRIPT NAME: ghost-blog.sh                  				*
+# *                                             				*
+# * VERSION: 1.0.2                              				*
+# *                                             				*
+# * DATE MODIFIED (MM-DD-YYYY): 06-07-2014      				*
+# *                                             				*
+# * AUTHOR: Atindriya Ghosh                     				*
+# *                                             				*
+# * FUNCTIONS:                                  				*
+# * 1. Start the blog                           				*
+# * 2. Stop the blog                            				*
+# * 3. Restart the blog                         				*
+# * 4. Deploy the latest release artifacts      				*
+# * 5. List all deployed artifacts      						*
+# * 6. Clean deployment path by removing specified artifact     *
+# ***************************************************************
 ## Functions
 usage(){
     echo "PARAMETERS:"
     echo "1. $(tput setaf 6)./ghost-blog.sh -s$(tput sgr 0) or $(tput setaf 6)./ghost-blog.sh --start$(tput sgr 0)$(tput sgr 0) - Starts the blog"
     echo "2. $(tput setaf 6)./ghost-blog.sh -t$(tput sgr 0) or $(tput setaf 6)./ghost-blog.sh --terminate$(tput sgr 0)$(tput sgr 0) - Stops the blog"
 	echo "3. $(tput setaf 6)./ghost-blog.sh -r$(tput sgr 0) or $(tput setaf 6)./ghost-blog.sh --restart$(tput sgr 0)$(tput sgr 0) - Restarts the blog"
-	echo "4. $(tput setaf 6)./ghost-blog.sh -d <Artifact Path> <Release Version>$(tput sgr 0) or $(tput setaf 6)./ghost-blog.sh --deploy <Artifact Path> <Release Version>$(tput sgr 0)$(tput sgr 0) - Deploy the latest release"
+	echo "4. $(tput setaf 6)./ghost-blog.sh -d <Artifact Path> <Release Version> <Deployment Path>$(tput sgr 0) or $(tput setaf 6)./ghost-blog.sh --deploy <Artifact Path> <Release Version> <Deployment Path>$(tput sgr 0)$(tput sgr 0) - Deploy the latest release"
+	echo "5. $(tput setaf 6)./ghost-blog.sh -l <Deployment Path>$(tput sgr 0) or $(tput setaf 6)./ghost-blog.sh --list <Deployment Path>$(tput sgr 0)$(tput sgr 0) - List all deployed artifacts"
+	echo "6. $(tput setaf 6)./ghost-blog.sh -c <Deployment Path> <Artifact Name>$(tput sgr 0) or $(tput setaf 6)./ghost-blog.sh --clean <Deployment Path> <Artifact Name>$(tput sgr 0)$(tput sgr 0) - Clean deployment path by removing specified artifact"
 }
 startup(){ 
 	echo "$(tput setaf 3)Starting The Blog ...$(tput sgr 0)"
@@ -44,13 +48,13 @@ restart(){
 deploy(){ 
 	shutdown
 	echo "$(tput setaf 3)Deploying Latest Release Artifacts ...$(tput sgr 0)"
-	dir_path="/var/www/content/themes/"
+	dir_path="$3"
 	dir_name="the-ghost-who-blogs-$2"
 	version="$2"
 	date=`date +"%Y-%m-%d %T"`
 	echo "$(tput setaf 3)Deployed Artifact Location = $dir_name$(tput sgr 0)"
 	cd $dir_path
-	sudo rm -rf the-ghost-who-blogs*
+	sudo rm -rf $dir_name
 	sudo mkdir $dir_name
 	cd $dir_name
 	sudo unzip "$1"
@@ -60,6 +64,25 @@ deploy(){
 	cd /var/www
 	echo "$(tput setaf 2)Release Artifacts Deployed Successfully$(tput sgr 0)"
 	startup
+}
+list(){
+	dir_path="$1"
+	artifacts=`ls -l $dir_path | egrep '^d' | awk '{print $9}'`
+	echo "$(tput setaf 3)Currently deployed artifacts$(tput sgr 0)"
+	count=1
+	for artifact in $artifacts
+	do
+		echo "  $(tput setaf 4)$count.$(tput sgr 0) $artifact"
+		count=$((count + 1))
+	done
+	
+}
+clean(){
+	dir_path="$1"
+	dir_name="$2"
+	echo "$(tput setaf 3)Cleaning artifact $dir_name ...$(tput sgr 0)"
+	sudo rm -rf $dir_path/$dir_name
+	echo "$(tput setaf 3)Cleaning successful$(tput sgr 0)"
 }
 ## Main
 while [ "$1" != "" ]; do
@@ -74,19 +97,37 @@ while [ "$1" != "" ]; do
 							exit
 							;;
 		"-d" | "--deploy"	)	
-							if ["$2" == "" || "$3" == ""]; 
+							if [ $# -eq 3 ]; 
 							then
-								echo "$(tput setaf 1)Please pass artifact path and release version$(tput sgr 0)\n"
+								echo "$(tput setaf 1)Please pass artifact path, release version and deployment path$(tput sgr 0)"
 								usage
 							else
-								deploy $2 $3
+								deploy $2 $3 $4
+							fi
+							exit
+							;;
+		"-l" | "--list"	)	if [ $# -eq 1 ]; 
+							then
+								echo "$(tput setaf 1)Please pass deployment path$(tput sgr 0)"
+								usage
+							else
+								list $2
+							fi
+							exit
+							;;
+		"-c" | "--clean")	if [ $# -eq 2 ]; 
+							then
+								echo "$(tput setaf 1)Please pass deployment path and artifact name$(tput sgr 0)"
+								usage
+							else
+								clean $2 $3
 							fi
 							exit
 							;;
 		"-h" | "--help"		)	usage
 							exit
 							;;
-		*					)	echo "$(tput setaf 1)Please pass the available parameters$(tput sgr 0)\n"
+		*					)	echo "$(tput setaf 1)Please pass the available parameters$(tput sgr 0)"
 							usage
 							exit
 	esac
